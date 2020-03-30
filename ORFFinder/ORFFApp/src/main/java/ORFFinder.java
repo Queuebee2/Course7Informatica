975e5c6c59ab140b27714e83fc245e48ac148a44
Milain Lambers <milain.lambers@gmail.com>
2020-03-30 19:05:20 +0200

    ORFFinder comment cleanup

--- ORFFinder/ORFFApp/src/main/java/ORFFinder.java
+++ ORFFinder/ORFFApp/src/main/java/ORFFinder.java
@@ -5,8 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 // try -Xms512M -Xmx512M in VM options
-public class ORFFinder {
 
+/**
+ * ORFFinder reads through a textfile (assumed format: nucleotide FASTA) to find ORFS
+ *
+ */
+public class ORFFinder   {
 
     static String filename_RELATIVE_TEMP = "data/DNA.txt";
 
@@ -26,44 +30,9 @@ public class ORFFinder {
     }
 
     /**
-     * ASCII TABLE
-     * Dec  Char                           Dec  Char     Dec  Char     Dec  Char
-     * ---------                           ---------     ---------     ----------
-     *   0  NUL (null)                      32  SPACE     64  @         96  `
-     *   1  SOH (start of heading)          33  !         65  A         97  a
-     *   2  STX (start of text)             34  "         66  B         98  b
-     *   3  ETX (end of text)               35  #         67  C         99  c
-     *   4  EOT (end of transmission)       36  $         68  D        100  d
-     *   5  ENQ (enquiry)                   37  %         69  E        101  e
-     *   6  ACK (acknowledge)               38  &         70  F        102  f
-     *   7  BEL (bell)                      39  '         71  G        103  g
-     *   8  BS  (backspace)                 40  (         72  H        104  h
-     *   9  TAB (horizontal tab)            41  )         73  I        105  i
-     *  10  LF  (NL line feed, new line)    42  *         74  J        106  j
-     *  11  VT  (vertical tab)              43  +         75  K        107  k
-     *  12  FF  (NP form feed, new page)    44  ,         76  L        108  l
-     *  13  CR  (carriage return)           45  -         77  M        109  m
-     *  14  SO  (shift out)                 46  .         78  N        110  n
-     *  15  SI  (shift in)                  47  /         79  O        111  o
-     *  16  DLE (data link escape)          48  0         80  P        112  p
-     *  17  DC1 (device control 1)          49  1         81  Q        113  q
-     *  18  DC2 (device control 2)          50  2         82  R        114  r
-     *  19  DC3 (device control 3)          51  3         83  S        115  s
-     *  20  DC4 (device control 4)          52  4         84  T        116  t
-     *  21  NAK (negative acknowledge)      53  5         85  U        117  u
-     *  22  SYN (synchronous idle)          54  6         86  V        118  v
-     *  23  ETB (end of trans. block)       55  7         87  W        119  w
-     *  24  CAN (cancel)                    56  8         88  X        120  x
-     *  25  EM  (end of medium)             57  9         89  Y        121  y
-     *  26  SUB (substitute)                58  :         90  Z        122  z
-     *  27  ESC (escape)                    59  ;         91  [        123  {
-     *  28  FS  (file separator)            60  <         92  \        124  |
-     *  29  GS  (group separator)           61  =         93  ]        125  }
-     *  30  RS  (record separator)          62  >         94  ^        126  ~
-     *  31  US  (unit separator)            63  ?         95  _        127  DEL
-     *
+     * ASCII TABLE (for reference)
      * using ordinal values as keys maybe? later?: ATG 658471, TAG 846571, TAA 846565, TGA 847165 (changed use of hashmaps to arraylist for now)
-     * chars of interest: (65, A) (84, T) (67, C) (71, G)
+     * chars of interest and their ASCII values: (65, A) (84, T) (67, C) (71, G) (62, >)
      */
     public void read() {
 
@@ -76,19 +45,17 @@ public class ORFFinder {
         Sequence currentSequence = null;
         boolean isStopCodon;
 
-        try (BufferedReader reader = Files.newBufferedReader(Path.of(filename_RELATIVE_TEMP))) {
-            int c;
+        int c;
+
+        try (BufferedReader reader = Files.newBufferedReader(
+                Path.of(filename_RELATIVE_TEMP))) {
+
             while ((c = reader.read()) != -1) {
-
-                // header 62 = >
-                // assume all files start with this
-
-                // chars of interest: (65, A) (84, T) (67, C) (71)
-
+                // iterate over character (int) in the file...
                 switch (c) {
-                    case (62):  // >
+                    case (62):  // ASCII 62 == >
                         double dnaStartTime = System.nanoTime();
-                        String header = reader.readLine(); // catch header and skip this header TODO confirm? CONFIRMED!
+                        String header = reader.readLine(); // Save the header
                         currentSequenceID++;
                         currentPos = 0;
                         if (currentSequence != null) {
@@ -108,10 +75,10 @@ public class ORFFinder {
                         currentSequence = new Sequence(currentSequenceID);
                         System.out.println("created " + currentSequenceID);
                         break;
-                    case (65):  // A
-                    case (67):  // C
-                    case (71):  // G
-                    case (84):  // T
+                    case (65):  // ASCII 65 == A
+                    case (67):  // ASCII 67 == C
+                    case (71):  // ASCII 71 == G
+                    case (84):  // ASCII 84 == T            // TODO enhancement add 'U' ?
                         currentPos++;
                         last[0] = last[1];
                         last[1] = last[2];
