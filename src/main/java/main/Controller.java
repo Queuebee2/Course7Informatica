02f48c41480abda9712f82dfee0a8685833dc217
Milain Lambers <milain.lambers@gmail.com>
2020-04-07 23:33:23 +0200

    Revert "merge ORFFinder with ORFvis & Maven structure (part 1) "
    This reverts commit bba402b6df26690369a47a989947155afe6c3e40.

--- src/main/java/main/Controller.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class Controller {
-
-    public Controller() {
-
-    }
-}
--- src/main/java/main/Settings.java
+++
@@ -1,5 +0,0 @@
-public class Settings {
-
-    public static final int MINIMAL_ORF_LENGTH = 0;       // used in Sequence to determine whether to keep an ORF or not
-
-}
