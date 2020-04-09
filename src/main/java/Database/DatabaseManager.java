320463a4319586241489c724ef8563a8022be941
Milain Lambers <milain.lambers@gmail.com>
2020-04-09 03:29:29 +0200

    some todo notes on database

---
+++ src/main/java/Database/DatabaseManager.java
@@ -0,0 +1,17 @@
+package Database;
+
+
+/**
+ *
+ * Todo : everything
+ * plan for now : implement Java DB? (??) -- embedded derby (only needs JDK, no server)
+ *      stuff needed for plan:
+ *          - A DB setup script
+ *          - methods to query db
+ */
+public class DatabaseManager {
+
+    public DatabaseManager() {
+
+    }
+}
