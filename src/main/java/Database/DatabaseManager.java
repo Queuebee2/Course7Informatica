97ea076872dd5f695980b47e2de46ba62e4b291c
Milain Lambers <milain.lambers@gmail.com>
2020-04-09 18:22:29 +0200

    Refactor 'Sequence' as 'FastaSequence'
    and removed redundant empty BlastManager, moved BlastManager up

--- src/main/java/Database/DatabaseManager.java
+++ src/main/java/Database/DatabaseManager.java
@@ -1,6 +1,9 @@
 package Database;
 
 
+import java.sql.*;
+import java.util.Properties;
+
 /**
  *
  * Todo : everything
@@ -10,9 +13,96 @@ package Database;
  *          - methods to query db
  */
 public class DatabaseManager {
+    private Connection connection;
 
-    public DatabaseManager() {
-
+    public DatabaseManager() throws SQLException {
+        makeConnection();
     }
 
+    public void makeConnection() throws SQLException {
+        connection = null;
+
+        // Initialize connection object
+        try {
+            String host = "hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com";
+            String database = "owe7_pg1";
+            String url = String.format("jdbc:mariadb://%s/%s", host, database);
+
+            // Set connection properties.
+            Properties properties = new Properties();
+            String user = "owe7_pg1@hannl-hlo-bioinformatica-mysqlsrv";
+            properties.setProperty("user", user);
+            String password = "password1";
+            properties.setProperty("password", password);
+            properties.setProperty("useSSL", "true");
+            properties.setProperty("verifyServerCertificate", "true");
+            properties.setProperty("requireSSL", "false");
+
+            // get connection
+            connection = DriverManager.getConnection(url, properties);
+        } catch (SQLException e) {
+            throw new SQLException("Failed to create connection to database", e);
+        }
+        if (connection != null) {
+            System.out.println("Successfully created connection to database.");
+        }
+        else{
+            System.out.println("Failed to create connection to database.");
+        }
+    }
+    public void insert() throws SQLException {
+        int nRowsInserted = 0;
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO test (name, quantity) VALUES (?, ?);");
+        preparedStatement.setString(1, "banana");
+        preparedStatement.setInt(2, 150);
+        nRowsInserted += preparedStatement.executeUpdate();
+
+        preparedStatement.setString(1, "orange");
+        preparedStatement.setInt(2, 154);
+        nRowsInserted += preparedStatement.executeUpdate();
+
+        preparedStatement.setString(1, "apple");
+        preparedStatement.setInt(2, 100);
+        nRowsInserted += preparedStatement.executeUpdate();
+        System.out.println(String.format("Inserted %d row(s) of data.", nRowsInserted));
+    }
+    public void download() throws SQLException {
+        try {
+
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * from test;");
+            while (results.next()) {
+                String outputString =
+                        String.format(
+                                "Data row = (%s, %s)",
+                                results.getString(1),
+                                results.getString(2));
+
+                System.out.println(outputString);
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Encountered an error when executing given sql statement", e);
+        }
+        System.out.println("Execution finished.");
+    }
+    public void update() throws SQLException {
+        try
+        {
+            // Modify some data in table.
+            int nRowsUpdated = 0;
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE test SET quantity = ? WHERE name = ?;");
+            preparedStatement.setInt(1, 200);
+            preparedStatement.setString(2, "banana");
+            nRowsUpdated += preparedStatement.executeUpdate();
+            System.out.println(String.format("Updated %d row(s) of data.", nRowsUpdated));
+
+            // NOTE No need to commit all changes to database, as auto-commit is enabled by default.
+        }
+        catch (SQLException e)
+        {
+            throw new SQLException("Encountered an error when executing given sql statement.", e);
+        }
+        System.out.println("Execution finished.");
+    }
 }
+
---
+++ src/main/java/orffinder/FastaSequence.java
@@ -0,0 +1,136 @@
+package orffinder;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FastaSequence implements Iterable<ORF> {
+
+    final String header;
+    final int SequenceID;
+    private static int IDIncrement = 0;
+    final long lineNumber;
+    final long offset;      // in file
+    public long EndPos;
+    public long RealSize;
+    private ArrayList<String[]> tablelist;
+
+    private  ArrayList<ORF> ORFList = new ArrayList<ORF>();
+    private  ArrayList<ORF>[] ORFTrackers =  new ArrayList[3];
+//final ArrayList<ArrayList<ORF>> ORFTrackers = new ArrayList < ArrayList < ORF >> (3); // if above doesnt work
+
+    public FastaSequence(String currHeader, int currentTextLine, int position) {
+        SequenceID = IDIncrement++;
+        header = currHeader;
+        lineNumber = currentTextLine;
+        offset = position; // in file
+        ORFTrackers[0]= new ArrayList<ORF>();
+        ORFTrackers[1]= new ArrayList<ORF>();
+        ORFTrackers[2]= new ArrayList<ORF>();
+    }
+
+    public void addNewORF(long position, long charCounter, int modulo) {
+        //ORFTrackers.get(modulo).add(new ORF(position, charCounter)); // for nested arraylist
+
+        ORFTrackers[modulo].add(new ORF(position, charCounter, this));
+    }
+
+    public void updateORFs(long endPos, long charCounter, int modulo) {
+        //ArrayList<ORF> tracker = ORFTrackers.get(modulo); // for nested arraylist
+        assert (modulo >= 0 && modulo <= 2) : "MODULO CANT BE < 0 or > 2";
+        ArrayList<ORF> tracker = ORFTrackers[modulo];
+        if (tracker.size() > 0) {
+            for ( ORF orf : tracker ) {
+                orf.endpos = endPos;
+                orf.counterEnd = charCounter;
+
+            }
+            ORFList.addAll(tracker);
+            tracker.clear();
+        }
+    }
+
+    public long getRealSize() {
+        return RealSize;
+    }
+
+    @Override
+    public Iterator<ORF> iterator() {
+        return ORFList.iterator();
+    }
+    public ArrayList<String[]> makeTable_list(){
+        tablelist = new ArrayList<String[]>();
+        int orfIdMaker = 0;
+        for(ORF orf:ORFList){
+            String[] orfvalue = new String[5];
+            orfvalue[0] = String.valueOf(SequenceID);
+            orfvalue[1] = String.valueOf(orf.getCounterStart());
+            orfvalue[2] = String.valueOf(orf.counterEnd);
+            orfvalue[3] = String.valueOf(orf.getSize());
+            orfvalue[4] = String.valueOf(orf.getID());
+            tablelist.add(orfvalue);
+        }
+        return tablelist;
+    }
+    public String getStatistics() {
+        long totalOrfLength = 0;
+        long averageOrfLength = 0;
+        long shortestOrfLength= 0;
+        long longestOrfLength= 0;
+        int completedOrfCount = 0;
+        int incompleteOrfCount = 0;
+
+        long size;
+
+        for (ArrayList<ORF> tracker : ORFTrackers) {
+            incompleteOrfCount += tracker.size();
+        }
+
+        for (ORF  orf: ORFList ) {
+            completedOrfCount++;
+            size =  orf.getSize();
+           totalOrfLength += size;
+
+            if (orf.getEndpos() != 0 && size < shortestOrfLength) {
+                shortestOrfLength = size;
+            }
+            if (size > longestOrfLength) {
+                longestOrfLength = size;
+            }
+        }
+        averageOrfLength = totalOrfLength / completedOrfCount;
+
+        StringBuilder statistics = new StringBuilder("DNA header=");
+        statistics.append(header);
+        statistics.append("{ID=").append(SequenceID);
+        statistics.append(" StartPos=").append(offset);
+        statistics.append(" EndPos=").append(EndPos);
+        statistics.append(" CalculatedSize=").append(EndPos - offset);
+        statistics.append(" RealSize=").append(RealSize);
+
+
+        for (int i = 0; i < 10; i++) {
+            ORF o = ORFList.get(i);
+            statistics.append("\n\torf startpos=").append(o.getOffset());
+            statistics.append(" endpos=").append(o.endpos);
+            statistics.append(" RealSize=").append(o.getSize());
+            statistics.append(" StartCounter=").append(o.getCounterStart());
+            statistics.append(" EndCounter=").append(o.counterEnd);
+            statistics.append(" ID: ").append(o.getID());
+            statistics.append(" PARENT: ").append(o.parentSequence);
+
+        }
+
+        statistics.append(" totalOrfLength=").append(totalOrfLength);
+        statistics.append(" averageOrfLength=").append(averageOrfLength);
+        statistics.append(" longestOrfLength=").append(longestOrfLength);
+        statistics.append(" shortestOrfLength=").append(shortestOrfLength);
+        statistics.append(" incompleteOrfCount=").append(incompleteOrfCount);
+        statistics.append(" completedOrfCount=").append(completedOrfCount);
+        statistics.append("}");
+
+        System.out.println(statistics.toString());
+        return statistics.toString();
+
+    }
+}
+
